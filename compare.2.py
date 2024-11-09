767027f1a58298099e65370c11287d60ad8d5b9f
Ioan Chiurciu <ioan@chiurciu.com>
2024-11-09 22:47:27 +0200

    finalizare pe astazi

---
+++ compare.2.py
@@ -0,0 +1,10 @@
+x = int(input("What is the number x? "))
+y = int(input("what is the number y? "))
+
+if x == y:
+    print("x equals y")
+else:
+    print("x does not equal y")
+
+
+#aici termin pe astazi, videoclipul la minutul 24,17
